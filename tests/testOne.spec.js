a3d01cf2ed686b75667e1507b0c6cae9df5e4bd2
Mark Nicoll <markjnicoll@gmail.com>
2025-02-27 13:54:35 +0000

    Update - Updated the tests
    Removed duplicated steps and added these to helper files.

--- tests/testOne.spec.js
+++ tests/testOne.spec.js
@@ -1,19 +1,18 @@
-// @ts-check
+// @ts-nocheck
 import { test, expect } from '@playwright/test';
 
-test('has title Dynamic Content', async ({ page }) => {
-	await page.goto("https://the-internet.herokuapp.com/dynamic_content?with_content=static");
+const { gotoDynamicContentPage } = require('./pages/navigate');
+const { performClicks } = require('../helpers/dynamicClick'); 
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle("Dynamic Content");
+test('Landing page has title Dynamic Content', async ({ page }) => {
+
+	await gotoDynamicContentPage(page);
+	await expect(page).to("Dynamic Content");
 });
 
-//test('get started link', async ({ page }) => {
-//  await page.goto('https://playwright.dev/');
 
-//  // Click the get started link.
-//  await page.getByRole('link', { name: 'Get started' }).click();
+test('Paragraph texts change except for the first paragraph', async ({ page }) => {
 
-//  // Expects page to have a heading with the name of Installation.
-//  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-//   });
+	await gotoDynamicContentPage(page);
+	await performClicks(page);
+});
