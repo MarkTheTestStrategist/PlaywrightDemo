7442113ae789c70ba6a5be4d815a71321b5911ca
Mark Nicoll <markjnicoll@gmail.com>
2025-02-28 12:14:35 +0000

    Add - New test
    Check for title and specific header

--- tests/testOne.spec.js
+++ tests/testOne.spec.js
@@ -1,18 +1,25 @@
 // @ts-nocheck
 import { test, expect } from '@playwright/test';
+const { Navigate } = require('../pages/navigate');
+const { performClicks } = require('../helpers/dynamicClick');
 
-const { gotoDynamicContentPage } = require('./pages/navigate');
-const { performClicks } = require('../helpers/dynamicClick'); 
+let navigator; // Declare outside tests
 
-test('Landing page has title Dynamic Content', async ({ page }) => {
-
-	await gotoDynamicContentPage(page);
-	await expect(page).to("Dynamic Content");
+test.beforeEach(async ({ page }) => {
+    navigator = new Navigate(page); // Instantiate before each test
 });
 
+test('Landing page has title The Internet', async ({ page }) => {
+    await navigator.toDynamicContent();
+    await expect(page).toHaveTitle("The Internet");
+});
+
+test('Landing page has header text "Dynamic Content', async ({ page }) => {
+    await navigator.toDynamicContent();
+    await expect(page.getByRole('heading', { name: 'Dynamic Content', level: 3 })).toBeVisible();
+});
 
 test('Paragraph texts change except for the first paragraph', async ({ page }) => {
-
-	await gotoDynamicContentPage(page);
-	await performClicks(page);
+    await navigator.toDashboardPage();
+    await performClicks(page);
 });
