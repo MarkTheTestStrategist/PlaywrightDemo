4379d862a5c85ff9c1959d5c0e198f4ab6d24063
Mark Nicoll <markjnicoll@gmail.com>
2025-03-05 08:34:05 +0000

    Add - Trace file code
    This includes a ps1 file with the relevant syntax for running the trace files. I have also included a trace file example

--- tests/testOne.spec.js
+++ tests/testOne.spec.js
@@ -1,12 +1,38 @@
 // @ts-nocheck
 import { test, expect } from '@playwright/test';
+import path from 'path';
+import fs from 'fs';
 const { Navigate } = require('../pages/navigate');
 const { assertTextVisibility } = require('../helpers/dynamicClick');
+const { WaitForPageToLoad } = require('../helpers/pageLoadStates');
 
-let navigator; // Declare outside tests
+let navigator;
 
-test.beforeEach(async ({ page }) => {
-    navigator = new Navigate(page); // Instantiate before each test
+test.beforeEach(async ({ page }, testInfo) => {
+    navigator = new Navigate(page);
+
+    await page.context().tracing.start({
+        title: `${testInfo.title}`,
+        screenshots: true,
+        snapshots: true,
+        sources: true
+    });
+});
+
+test.afterEach(async ({ page }, testInfo) => {
+    const playwrightTraces = 'playwright-traces';
+    const tracePath = path.join(
+        process.cwd(),
+        playwrightTraces,
+        `${testInfo.title}.zip`
+    );
+
+    // Ensure trace directory exists
+    if (!fs.existsSync(playwrightTraces)) {
+        fs.mkdirSync(playwrightTraces, { recursive: true });
+    }
+
+    await page.context().tracing.stop({ path: tracePath });
 });
 
 test('Landing page has title The Internet', async ({ page }) => {
@@ -19,7 +45,21 @@ test('Landing page has header text "Dynamic Content', async ({ page }) => {
     await expect(page.getByRole('heading', { name: 'Dynamic Content', level: 3 })).toBeVisible();
 });
 
-test('Paragraph texts change except for the first paragraph', async ({ page }) => {
-    await navigator.toDashboardPage();
+test('Paragraph text visiblity', async ({ page }) => {
+    await navigator.toDynamicContent();
     await assertTextVisibility(page);
+});
+
+test('Three images are present', async ({ page }) => {
+    await navigator.toDynamicContent();
+    await WaitForPageToLoad;
+    await expect(page.getByRole('img', { src = "/img/avatars/Original-Facebook-Geek-Profile-Avatar-7.jpg" }).nth(1)).toBeVisible();
+    await expect(page.getByRole('img', { src = "https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-7.jpg"}).nth(2)).toBeVisible();
+    await expect(page.getByRole('img', {src = ""}).nth(3)).toBeVisible();
+});
+
+test('Images have correct urls', async ({ page }) => {
+    await navigator.toDynamicContent();
+    await page.locator('img[src="/img/avatars/Original-Facebook-Geek-Profile-Avatar-7.jpg"]').toBeVisible;
+    await expect(page).toHaveURL('/img/avatars/Original-Facebook-Geek-Profile-Avatar-7.jpg');
 });
