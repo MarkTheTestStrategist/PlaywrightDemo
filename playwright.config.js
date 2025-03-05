3d39fbf63c581d3e3eb5e955c9035401ba0a00bc
Mark Nicoll <markjnicoll@gmail.com>
2025-03-05 13:59:02 +0000

    Refactor - Test for three images
    This now checks there are three images within the table and then ensures the urls exist in the img array in the constants.js file, as they are randomly displayed with every page load.

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
-        headless: false // Run tests in headed mode
+        headless: true // Run tests in headed mode
     },
 
     /* Configure projects for major browsers */
