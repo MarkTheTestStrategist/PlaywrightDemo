89e7aea8c0881e2b89ae580bd774dc2b695f68d9
Mark Nicoll <markjnicoll@gmail.com>
2025-03-05 08:35:20 +0000

    Refactor - Simplified this test

--- helpers/dynamicClick.js
+++ helpers/dynamicClick.js
@@ -1,32 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 module.exports = {
-    async performClicks(page) {
-        // Click on the first set of elements
+    async assertTextVisibility(page) {
         await expect(page.getByText('This example demonstrates the')).toBeVisible();
         await expect(page.getByText('To make some of the content')).toBeVisible();
         await expect(page.getByText('Accusantium eius ut')).toBeVisible();
-
-        await page.getByText('Omnis fugiat porro vero quas').click();
-        await page.getByText('Nihil eos ipsam architecto').click();
-        await page.getByRole('link', { name: 'click here' }).click();
-        await expect(page.getByText('Accusantium eius ut')).toBeVisible();
-
-        // Click on the second set of elements
-        await page.getByText('Omnis fugiat porro vero quas').click();
-        await page.getByText('Maiores autem magnam impedit').click();
-        await page.getByRole('link', { name: 'click here' }).click();
-        await expect(page.getByText('Accusantium eius ut')).toBeVisible();
-
-        // Click on the third set of elements
-        await page.getByText('Omnis fugiat porro vero quas').click();
-        await page.getByText('Non est molestiae aut et').click();
-        await page.getByRole('link', { name: 'click here' }).click();
-        await expect(page.getByText('Accusantium eius ut')).toBeVisible();
-
-        // Click on additional elements
-        await page.locator('div:nth-child(4)').click();
         await expect(page.getByText('Omnis fugiat porro vero quas')).toBeVisible();
-        await page.getByText('Ducimus sed ut minus velit').click();
     }
 };
